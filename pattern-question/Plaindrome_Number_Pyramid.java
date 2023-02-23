056fbc00c2084a0d07423d8b43a67cfce6da9c81
Rio Fairhall <74183413+RioFairhall@users.noreply.github.com>
2023-02-23 09:26:42 +0530

    Create Plaindrome_Number_Pyramid.java

---
+++ pattern-question/Plaindrome_Number_Pyramid.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Plaindrome_Number_Pyramid
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++)
+			{
+				//printing Spaces
+				for(int j=1; j<=n-i; j++)
+					{
+						System.out.print("  ");
+					}
+				//printing Numbers
+					//1st part- till 1 i.e. from i to 1
+				for(int j=i; j>0; j--)
+					{
+						System.out.print(j+" ");
+					}
+					//2nd part- from i to 2
+				for(int j=i; j>=2; j--)
+					{
+						System.out.print(j+" ");
+					}
+				System.out.println();
+			}
+		
+	}
+}
