9dfedd5b4e2f614d57a046467423dc358726d30f
RioFairhall <74183413+RioFairhall@users.noreply.github.com>
2023-02-23 06:52:48 +0530

    Update butterfly-pattern.java

--- pattern-question/butterfly-pattern.java
+++ pattern-question/butterfly-pattern.java
@@ -6,35 +6,44 @@ public class Main
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		//your code here
+		//input from user
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		//upper part
 		for(int i=0; i<n; i++)
 			{
+				//left part- printing Stars
 				for(int j=0; j<=i; j++)
 					{
 						System.out.print("*");
 					}
-				for(int j=0; j<2*(n-i-1); j++)
+				//Mid part- printing Spaces
+					for(int j=0; j<2*(n-i-1); j++)
 					{
 						System.out.print(" ");
 					}
+				//right part- printing Stars Again
 				for(int j=0; j<=i; j++)
 					{
 						System.out.print("*");
 					}
 				System.out.println();
 			}
+
+		//lower part
 		for(int i=n; i>0; i--)
 			{
+				//left part- prining Stars
 				for(int j=0; j<i; j++)
 					{
 						System.out.print("*");
 					}
+				//Mid part- printing Spaces
 				for(int j=0; j<2*(n-i); j++)
 					{
 						System.out.print(" ");
 					}
+				//Right part- printing Stars Again
 				for(int j=0; j<i; j++)
 					{
 						System.out.print("*");
